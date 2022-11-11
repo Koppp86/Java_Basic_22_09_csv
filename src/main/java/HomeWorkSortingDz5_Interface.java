9cb710490553483bb186779105472ab6f420e025
Koppp86 <vinokurov86@yandex.ru>
2022-11-11 09:57:45 +0300

    ДЗ_05_Урока11. Ревью.

--- src/main/java/HomeWorkSortingDz5_Interface.java
+++ src/main/java/HomeWorkSortingDz5_Interface.java
@@ -1,12 +1,11 @@
 package src.main.java;
 
-import java.util.Arrays;
-import java.util.Date;
+import java.util.*;
 
-public class HomeWorkSortingDz5 {
+public class HomeWorkSortingDz5_Interface {
     public static void main(String[] args) {
-        int[] array1 = new int[100_000];    // Массив для сортировки Выбором
-        int[] array2 = new int[100_000];    // Массив для Встроенной сортировки
+        List<Integer> list1 = new ArrayList<>();     // Массив для сортировки Выбором
+        List<Integer> list2 = new ArrayList<>();    // Массив для Встроенной сортировки
         int indexMin;
         int valueMin;
         long start1;
@@ -15,30 +14,31 @@ public class HomeWorkSortingDz5 {
         long end2;
 
         System.out.println("1) Наполнение Массивов.");
-        for (int i = 0; i < array1.length; i++) {
-            array1[i] = ((int)(Math.random() * 100_001));
-            array2[i] = ((int)(Math.random() * 100_001));
+        for (int i = 0; i < 100_000; i++) {
+            list1.add((int)(Math.random() * 100_001));
+            list2.add((int)(Math.random() * 100_001));
         }
+
         System.out.println("2.1) Сортировка Выбором ...");
         start1 = new Date().getTime();
-        for (int i = 0; i < array1.length-1; i++) {
+        for (int i = 0; i < list1.size()-1; i++) {
             indexMin = i;
-            valueMin = array1[i];
-            for (int j = i + 1; j < array1.length; j++) {
-                if (valueMin > array1[j]) {
-                    valueMin = array1[j];
+            valueMin = list1.get(i);
+            for (int j = i + 1; j < list1.size(); j++) {
+                if (valueMin > list1.get(j)) {
+                    valueMin = list1.get(j);
                     indexMin = j;
                 }
             }
-            array1[indexMin] = array1[i];
-            array1[i] = valueMin;
+            list1.set(indexMin, list1.get(i));
+            list1.set(i, valueMin);
         }
         end1 = new Date().getTime();
         System.out.println("2.2) Сортировка Выбором заняла " + (end1 - start1) + " ms");
 
         System.out.println("3.1) Встроенная сортировка ...");
         start2 = new Date().getTime();
-        Arrays.sort(array2);
+        Collections.sort(list2);
         end2 = new Date().getTime();
         System.out.println("3.2) Встроенная сортировка заняла " + (end2 - start2) + " ms");
     }
