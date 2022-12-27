3297b721a1a884c2a921d69da01f3f40c9467884
Koppp86 <vinokurov86@yandex.ru>
2022-12-27 11:38:12 +0300

    Ревью ДЗ_6 Урока_12 (Работа со счетами клиента банка).

--- src/main/java/workingWithBankAccountsDz6Lesson11/Bank.java
+++ src/main/java/workingWithBankAccountsDz6Lesson11/Bank.java
@@ -1,19 +1,20 @@
 package src.main.java.workingWithBankAccountsDz6Lesson11;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Bank {
     private String name;
     private Set<Client> clientSet;
     private Set<Account> accountSet;
+    private Map<Account, Client> accountClientMap;
+    private Map<Client, Account> clientAccountMap;
 
     public Bank(String name){
         this.name = name;
         this.clientSet = new HashSet<>();
         this.accountSet = new HashSet<>();
+        this.accountClientMap = new HashMap<>();
+        this.clientAccountMap = new HashMap<>();
     }
 
     public void addClient(Client client){
@@ -22,7 +23,18 @@ public class Bank {
     public void addAccount(Account account){
         accountSet.add(account);
     }
-    public List<Account> getAccounts(Client client) {
+    public void addAccountClient(Account account, Client client){
+        accountClientMap.put(account, client);
+        clientAccountMap.put(client, account);
+    }
+    public Account findAccountMap(Client client) {
+        return clientAccountMap.get(client);
+    }
+    public Client findClientMap(Account account){
+        return accountClientMap.get(account);
+    }
+
+    public List<Account> findAccounts(Client client) {
         List<Account> resultAccounts = new ArrayList<>();
         for (Account accountOther : accountSet) {
             if (client.equals(accountOther.getClient())) {
--- src/main/java/workingWithBankAccountsDz6Lesson11/WorkingWithBankAccountsDz6Lesson11.java
+++ src/main/java/workingWithBankAccountsDz6Lesson11/WorkingWithBankAccountsDz6Lesson11.java
@@ -29,8 +29,21 @@ public class WorkingWithBankAccountsDz6Lesson11 {
         bank1.addAccount(account31);
         bank1.addAccount(account41);
 
-        List<Account> clientAccounts = bank1.getAccounts(client2);   // 1) Найти все счета по клиенту2
-        Client foundClient = bank1.findClient(account21);            // 2) Найти клиента по счету21
+        bank1.addAccountClient(account11, client1);
+        bank1.addAccountClient(account12, client1);
+        bank1.addAccountClient(account13, client1);
+        bank1.addAccountClient(account21, client2);
+        bank1.addAccountClient(account22, client2);
+        bank1.addAccountClient(account31, client3);
+        bank1.addAccountClient(account41, client4);
+
+        List<Account> clientAccounts = bank1.findAccounts(client2);   // 1) Найти все счета по клиенту2
+        Client foundClient = bank1.findClient(account21);             // 2) Найти клиента по счету21
+
+        Account foundAccount = bank1.findAccountMap(client2);         // 1) Найти все счета по клиенту2 (Новый поиск_1 "О(1)")
+        System.out.println("0.1) Map. Вывод Счета Клиента2 : " + foundAccount.getNnAccount());
+        Client foundClient2 = bank1.findClientMap(account21);         // 2) Найти клиента по счету21 (Новый поиск_2 "О(1)")
+        System.out.println("0.2) Map. Вывод Клиента Счета21: " + foundClient2.getNameClient());
 
         System.out.println("1) Вывод Счетов Клиента2 Петя: ");
         for (Account accountOther : clientAccounts) {
@@ -40,7 +53,7 @@ public class WorkingWithBankAccountsDz6Lesson11 {
         System.out.println(foundClient.getNameClient());
 
         Client client5 = new Client("Вася", 15);
-        List<Account> clientAccounts2 = bank1.getAccounts(client5);   // 3) Найти все счета по клиенту5
+        List<Account> clientAccounts2 = bank1.findAccounts(client5);   // 3) Найти все счета по клиенту5
         System.out.println("3) Вывод Счетов Клиента5 Васи: ");
         for (Account accountOther : clientAccounts2) {
             System.out.println(accountOther.getNnAccount());
