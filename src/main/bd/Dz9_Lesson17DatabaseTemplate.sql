da36f8f768180c2026bd6ddbb82b64dd9fa2f744
Koppp86 <vinokurov86@yandex.ru>
2023-01-04 00:54:57 +0300

    Ревью ДЗ_9 Урока_17 (БД. Система тестирования)

--- src/main/bd/Dz9_Lesson17DatabaseTemplate.sql
+++ src/main/bd/Dz9_Lesson17DatabaseTemplate.sql
@@ -3,6 +3,18 @@
 -- -------------------------------
 create database Template;
 
+/*Не стоит хранить question_id в таблице с ответами - это нарушает нормализацию.
+  Лучше добавить таблицу со связями:
+  Таблица Answers:
+  id SERIAL PRIMARY KEY,
+  answer VARCHAR(100) NOT NULL UNIQUE
+
+  Таблица Question_to_answer:
+  question_id
+  answer _id
+
+  Т.е. реализуем связь многие-ко-многим. У одного вопроса может быть несколько ответов и один ответ может быть у нескольких разных вопросов.*/
+
 -- -------------------------------
 -- 1) Вопросы
 -- -------------------------------
@@ -29,31 +41,57 @@ order by question_nn;
 create table answers
 (
 	answer_id 	serial 		 primary key,
-	question_id integer 	 references questions (question_id),
-	answer		varchar(100)
+	answer		varchar(100) not null unique
 );
 
 insert into answers(question_id, answer)
 values
-	(1, '60'),
-	(1, '100'),
-	(1, '50'),
-	(2, '12'),
-	(2, '24'),
-	(2, '36'),
-	(2, '48'),
-	(3, '10'),
-	(3, '11'),
-	(3, '12'),
-	(3, '13'),
-	(3, '15');
+	('60'),
+	('100'),
+	('50'),
+	('12'),
+	('24'),
+	('36'),
+	('48'),
+	('10'),
+	('11'),
+	('13'),
+	('15');
 
-select answer_id, question_id, answer
+select answer_id, answer
 from answers
+order by answer_id;
+
+-- -------------------------------
+-- 3) Ответы на Вопросы
+-- -------------------------------
+create table question_to_answer
+(
+	question_id integer references questions (question_id),
+	answer_id   integer references answers (answer_id)
+);
+
+insert into question_to_answer(question_id, answer_id)
+values
+	(1, 1),
+	(1, 2),
+	(1, 3),
+	(2, 4),
+    (2, 5),
+    (2, 6),
+    (2, 7),
+    (3, 8),
+    (3, 9),
+    (3, 4),
+    (3, 10),
+	(3, 11);
+
+select question_id, answer_id
+from question_to_answer
 order by question_id, answer_id;
 
 -- -------------------------------
--- 3) Правильные ответы
+-- 4) Правильные ответы
 -- -------------------------------
 create table correctAnswers
 (
@@ -75,5 +113,6 @@ order by question_nn;
 -- -------------------------------
 select q.question_nn, q.question, a.answer, ca.answer as correctAnswer
 from questions q
-join answers a on a.question_id=q.question_id
+join question_to_answer qa on qa.question_id=q.question_id
+join answers a on a.answer_id=qa.answer_id
 join correctAnswers ca on ca.question_nn=q.question_nn;
